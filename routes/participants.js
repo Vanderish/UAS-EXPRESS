42bedfc49e031ef92cdd5f02fa3587ebe9cc5216
evan <mharisevan@gmail.com>
2026-05-29 11:01:29 +0700

    remove token for add participant

--- routes/participants.js
+++ routes/participants.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/:room_id', participantController.getParticipantsByRoomId);
 router.get('/qr/:room_id', auth.getToken, participantController.generateRoomQr);
-router.post('/:room_id', auth.getToken, participantController.addParticipant);
+router.post('/:room_id', participantController.addParticipant);
 router.delete('/:id', auth.getToken, participantController.deleteParticipant);
 router.put('/:id', auth.getToken, participantController.updateParticipant);
 
